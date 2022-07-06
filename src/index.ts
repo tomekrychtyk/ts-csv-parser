aa781a3606ebcffb1526cdabfcbceaac71e41341
Tomek <tomek@rychtyk.pl>
2022-07-06 18:33:02 +0200

    WIP: parsing the CSV file

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
-console.log('fff');
+import fs from 'fs'
+
+const matches = fs.readFileSync('football.csv', {
+  encoding: 'utf-8'
+}).split('\n').map((row: string): string[] => {
+  return row.split(',');
+});
+
+console.log(matches);
